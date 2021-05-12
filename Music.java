7a9c0a7b173279e2738ef01dc9d5519695da5ae9
jymeraz <60449001+jymeraz@users.noreply.github.com>
2021-05-12 10:51:01 -0600

    Add files via upload

---
+++ Music.java
@@ -0,0 +1,29 @@
+/* CS 1101 Intro to Computer Science 
+*Instructor: Logan 
+*Music
+*Modified and Submitted by: Janeth Meraz
+*/
+
+
+public class Music{
+    public static void main(String []args){
+        String name1 = "Relaxing";
+        String name2 = "Janeth";
+        String name3 = "Talk";
+        String name4 = "Khalid";
+        String month = "September";
+        int day = 9;
+        int year = 2019;
+        char money = '$';
+        double cost = 1.29;
+        
+        System.out.print("This is your " + name1 + " playlist!"); 
+        System.out.print("\n");
+        System.out.print("" + name2 + "'s favorite song is " + name3 + " by: " + name4 + "");
+        System.out.print("\n");
+        System.out.print("Today's Date:\t" + month + "/" + day + "/" + year + " ");
+        System.out.print("\n");
+        System.out.print("The song costs:\t" + money + "" + cost + " online.");
+        System.out.print("\n");
+    }   
+}
