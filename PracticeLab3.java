6293d2dd1ad4578ac9fa932c8d19f9222fc4e512
jymeraz <60449001+jymeraz@users.noreply.github.com>
2021-05-12 10:50:25 -0600

    Add files via upload

---
+++ PracticeLab3.java
@@ -0,0 +1,21 @@
+/* CS 1101 Intro to Computer Science 
+*Instructor: Logan 
+*Lab 1 Introduction
+*Modified and Submitted by: Janeth Meraz
+*/
+
+
+public class PracticeLab3{
+    public static void main(String []args){
+        String name = "Janeth Meraz";
+        int age = 19; 
+        char eP = '!';
+        
+        System.out.print("My name is " + name); 
+        System.out.print("\n\tI am a Junior, studying Mathematics.");
+        System.out.print("\n\t\tI went to El Dorado High School, and");
+        System.out.print("\n\t\t\tI am " + age + " years old.");
+        System.out.print("\n\t\t\t\tI like to paint" + eP);
+        System.out.print("\n");
+    }   
+}
