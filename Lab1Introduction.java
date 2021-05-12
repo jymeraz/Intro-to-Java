bff2bcdca384eee6dcc0636772583a3afec723f0
jymeraz <60449001+jymeraz@users.noreply.github.com>
2021-05-12 10:51:26 -0600

    Add files via upload

---
+++ Lab1Introduction.java
@@ -0,0 +1,12 @@
+/* CS 1101 Intro to Computer Science 
+*Instructor: Logan 
+*Lab 1 Introduction
+*Modified and Submitted by: Janeth Meraz
+*/
+
+
+public class Lab1Introduction{
+    public static void main(String []args){
+        System.out.print("My name is Janeth Meraz!\n\tI am a Junior, studying Mathematics.\n\t\tI went to El Dorado High School, and\n\t\t\tI am 19 years old.\n\t\t\t\tI like to paint!\n");
+    }   
+}
