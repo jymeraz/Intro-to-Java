83b5a13ed33a96cab5ad5e111593bd897365cb9f
jymeraz <60449001+jymeraz@users.noreply.github.com>
2021-05-12 10:50:42 -0600

    Add files via upload

---
+++ MyFirstLab.java
@@ -0,0 +1,13 @@
+/* CS 1101 Intro to Computer Science 
+*Instructor: Logan 
+*First Lab Assignment 
+*Modified and Submitted by: Janeth Meraz
+*/
+
+
+public class MyFirstLab{
+    public static void main(String []args){
+        System.out.println("Hello \t World!");
+        System.out.println("\nThis is my first program.");
+    }   
+}
